6337f26715ad9afe1bdbf51ff5cfd3bc0a9f38cb
Andras Danko <andras.danko@dataracing.com>
2025-12-14 22:44:24 +0100

    staging build

--- assets/chunks/chunk-CV18yBHL.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./chunk-Bz7S3e5o.js";function o(){return a()?.data}export{o as u};
---
+++ assets/chunks/chunk-CwnmU_Om.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chunk-BGmof1nQ.js";function o(){return a()?.data}export{o as u};
